c2d1182e68e32b0e2ab650c818aeff4a71bc26be
CamiloZuleta <camilozv@gmail.com>
2022-11-20 15:09:00 -0500

    First Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using API.Data;
+using API.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() => await _context.Users.ToListAsync();
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<AppUser>> GetUsersById(int Id) => await _context.Users.FindAsync(Id);
+    }
+}
